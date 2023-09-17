7e4bd78c4f936689aed6054495fe2eedb195b403
pavankumarkona <pavan.kumar@blurgs.com>
2023-09-18 00:09:27 +0530

    react configured

---
+++ pdftowordapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PdftowordappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pdftowordapp'
---
+++ static/js/pdftowordapp/homepage.js
@@ -0,0 +1,10 @@
+
+function App() {
+    
+    return (
+        
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
